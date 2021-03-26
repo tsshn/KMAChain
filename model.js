060bdaa17d8b0ced3d1203e6f78cfec819ae14e3
AndryKovalenko <andry.kovalenko@gmail.com>
2021-03-26 14:55:18 +0200

    Init

---
+++ model.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "email": {
+        "fullname": "Student",
+        "speciality": "IPZ",
+        "faculty": "FI",
+        "trims": [
+            {
+                "subjects": {
+                     // subject_name - OOP etc.
+                    "subject_name":
+                        {
+                           //or maybe teacher object
+                            "teacher": "textext",
+                            // subject mark
+                            "mark": 100,
+                           
+                            "themes": {
+                                "theme_name":
+                                    {
+                                        "name": "texttext",
+                                        "mark": 6,
+                                        //MS1970
+                                        "date": 2315325231
+                                    }
+                            },
+
+
+                        }
+                }
+            }
+        ]
+    }
+};
